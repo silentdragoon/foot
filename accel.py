52bea42d7f5fcad6ae9b43b4cbc6efeaeaf09142
William Judd <silentdragoon@gmail.com>
2011-02-13 19:01:11 +0000

    Added smoothing, stationary detection, and initial config.

--- accel.py
+++ accel.py
@@ -14,15 +14,21 @@ def heardEnter():
             return True
     return False
 
+def avg(values):
+	return sum(values, 0.0) / len(values)
+
 
 ser = serial.Serial('/dev/ttyACM0',115200,timeout=1)
 
 #Start access point
 ser.write(startAccessPoint())
 
-hx = 0
-hy = 0
-hz = 0
+counter = 0
+configcount = 0
+normal = []
+xlog = []
+ylog = []
+zlog = []
 
 while 1:
     #Send request for acceleration data
@@ -33,17 +39,51 @@ while 1:
 	x = ord(accel[0])
 	y = ord(accel[1])
 	z = ord(accel[2])
-        print "x: " + str(x) + " y: " + str(y) + " z: " + str(z)
-	if (abs(x) > abs(hx)):
-		hx = x
-	if (y > hy):
-		hy = y
-	if (z > hz):
-		hz = z
+
+	x -= 128
+	y -= 128
+	z -= 128
+
+	xlog.append(x)
+	ylog.append(y)
+	zlog.append(z)
+
+	counter += 1
+	configcount += 1
+
+	# average last five movements	
+
+
+	if configcount < 100:
+		counter = 0
+	
+	if configcount == 100:
+		counter = 0
+		normal.extend([round(avg(xlog)),round(avg(ylog)),round(avg(zlog))])
+		print "Stationary position: " + str(normal)
+		
+
+	if counter == 5:
+		counter = 0
+		px = round(avg(xlog)) - normal[0]
+		py = round(avg(ylog)) - normal[1] 
+		pz = round(avg(zlog)) - normal[2] 
+
+		if (abs(px) + abs(py) + abs(pz)) < 20:
+			print "Stationary."
+		else:
+			print "x: " + str(px) + " y: " + str(py) + " z: " + str(pz)
+
+
+		del xlog[:]
+		del ylog[:]
+		del zlog[:]
+
+	# if motion is detected, print motion
+	# if not much motion is detection, print stationary
 
     if heardEnter():
-	print "Thanks for playing! High scores:"
-	print "x: " + str(hx) + " y: " + str(hy) + " z: " + str(hz)
+	print "Thanks for playing!"
 	ser.close()
 	sys.exit(0)
 ser.close()
